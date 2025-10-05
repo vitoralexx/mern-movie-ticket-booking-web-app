0fb500826cfea39e2ae67fcb61b89ad8346a1393
vitoralexx <vitoralexse@gmail.com>
2025-10-05 19:27:16 -0300

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,45 @@
-import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+//
+import React from "react";
+
+//
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+//
+import Navbar from "./components/layout/Navbar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
 
 const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+
   return (
     <BrowserRouter>
+      <Toaster />
+      <Navbar />
+      {!isAdminRoute && <Navbar />}
       <Routes>
-        <Route path={"/"} element={ } />
-     </Routes>
+
+        {/* favorites */}
+        <Route path={"/favorite"} element={<Favorite />} />
+
+        {/* home */}
+        <Route path={"/"} element={<Home />} />
+
+        {/* movies */}
+        <Route path={"/movies"} element={<Movies />} />
+        <Route path={"/movies/:id"} element={<MoviesDetails />} />
+        <Route path={"/movies/:id/:date"} element={<SeatLayout />} />
+
+        {/* my-bookings */}
+        <Route path={"/my-bookings"} element={<MyBookings />} />
+
+        {/**/}
+
+      </Routes>
     </BrowserRouter>
-  )
+  );
 };
 
 export default App;
