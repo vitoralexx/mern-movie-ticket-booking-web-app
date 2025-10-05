9d9cea9fcba276745413e4ad0e8e356aa0bce0aa
vitoralexx <vitoralexse@gmail.com>
2025-10-05 12:42:25 -0300

    commit

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div
+      className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5"
+    >
+      <Link>
+        <img src={assets.logo} className="w-36 h-auto" alt="" />
+      </Link >
+    </div>
   )
 }
 
