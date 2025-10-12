82e2fb536e02bc2581b99092a39bd0a735eb5d06
vitoralexx <vitoralexse@gmail.com>
2025-10-12 20:06:53 -0300

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+//
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import SeatLayout from "./pages/SeatLayout";
+import MyBookings from "./pages/MyBookings";
+import Favorite from "./pages/Favorite";
+import Footer from "./components/layout/Footer";
+
+//
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+};
 
 export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Homes</div>
+    <div>Home</div>
   )
 }
 
