06cfa0e917cb12d45d411144c8fdc1cc1215e0a9
vitoralexx <vitoralexse@gmail.com>
2025-10-15 22:20:06 -0300

    commit

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,25 +1,87 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { assets } from '../../assets/assets';
-import { Menu } from 'lucide-react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets";
+import { Menu, MenuIcon, SearchIcon, XIcon } from "lucide-react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
-      <Link to="/">
-        <img src={assets.logo} className="w-36 h-auto" alt="" />
+    <div className="fixed top-0 left-0 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
+
+      <Link to="/" className="max-md:flex-1">
+        <img src={assets.xoxo} className="w-20 h-auto" alt="" />
       </Link>
 
-      <div>
+      <div
+        className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop:blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${
+          isOpen ? "max-md:w-full" : "max-md:w-0"
+        }`}
+      >
+        <XIcon
+          className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}
+        />
+
+        <Link
+          onClick={() => {
+            scrollTo(0, 0), setIsOpen(false);
+          }}
+          to={"/"}
+        >
+          Home
+        </Link>
+        <Link
+          onClick={() => {
+            scrollTo(0, 0), setIsOpen(false);
+          }}
+          to={"/movies"}
+        >
+          Movies
+        </Link>
+        <Link
+          onClick={() => {
+            scrollTo(0, 0), setIsOpen(false);
+          }}
+          to={"/"}
+        >
+          Theaters
+        </Link>
+        <Link
+          onClick={() => {
+            scrollTo(0, 0), setIsOpen(false);
+          }}
+          to={"/"}
+        >
+          Releases
+        </Link>
+        <Link
+          onClick={() => {
+            scrollTo(0, 0), setIsOpen(false);
+          }}
+          to={"/favorite"}
+        >
+          Favorites
+        </Link>
       </div>
 
-      <div>
+      <div className="flex items-center gap-8">
+        <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
+        {!user ? (
+          <button className="px-4 py-1 sm:px-7 sm:py-2 bg-amber-500 text-white transition rounded-full font-medium cursor-pointer">
+            Login
+          </button>
+        ) : (
+          <UserButton />
+        )}
       </div>
 
-      <Menu className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" />
-
+      <MenuIcon
+        className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
